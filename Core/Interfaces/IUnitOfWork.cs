109cd2415d38282c1b687def0753a1e796d062d2
DeLxCruz <lopezsantiago3503@gmail.com>
2023-10-20 12:11:06 -0500

    feat: :wrench: Update Interfaces

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -7,6 +7,34 @@ namespace Core.Interfaces
 {
     public interface IUnitOfWork
     {
+        ICargo Cargos { get; }
+        ICliente Clientes { get; }
+        IColor Colores { get; }
+        IDepartamento Departamentos { get; }
+        IDetalleOrden DetalleOrdenes { get; }
+        IDetalleVenta DetallesVentas { get; }
+        IEmpleado Empleados { get; }
+        IEmpresa Empresas { get; }
+        IEstado Estado { get; }
+        IFormaPago FormasPago { get; }
+        IGenero Generos { get; }
+        IInsumo Insumos { get; }
+        IInsumoPrenda InsumosPrendas { get; }
+        IInsumoProveedor InsumosProveedores { get; }
+        IInvenatario Inventarios { get; }
+        IInventarioTalla InventarioTallas { get; }
+        IMunicipio Municipios { get; }
+        IOrden Ordenes { get; }
+        IPais Paises { get; }
+        IPrenda Prendas { get; }
+        IProveedor Proveedores { get; }
+        ITalla Tallas  { get; }
+        ITipoEstado TipoEstados { get; }
+        ITipoPersona TipoPersonas { get; }
+        ITipoProteccion TipoProtecciones { get; }
+        IVenta Ventas { get; }
+
+
         Task<int> SaveAsync();
     }
 }
