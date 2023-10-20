6313a477896daba8f0b30d388c48a01650bccdd0
DeLxCruz <lopezsantiago3503@gmail.com>
2023-10-20 08:04:54 -0500

    feat: :tada: Begin a Project

---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using.Infrastructure.Data;
+using Core.Interfaces;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitoOfWork, IDisposable
+    {
+        private readonly RopaApiContext _context;
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
